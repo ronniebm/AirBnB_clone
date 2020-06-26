0ae9627db5f508f82997474d4543afb70d381fbf
ronniebm <ronnie.coding@gmail.com>
2020-06-26 13:17:35 -0500

    task3 complete and working v1.1

--- models/base_model.py
+++ models/base_model.py
@@ -26,17 +26,28 @@ class BaseModel():
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """str representation of the base class"""
+        """str representation of the base class."""
         msg = ("[{}] ({}) {}".format(self.__class__.__name__,
                self.id, self.__dict__))
         return msg
 
     def save(self):
+        """
+        This method updates the instance attribute
+        'self.udated_at'.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        This method creates a new dictionary:
+        -------------------------------------
+        Key: {created_at} -- was converted to string isoformat().
+        Key: {updated_at} -- was converted to string isoformat().
+        Key: {__class__}  -- was added, it contains the class name.
+        """
         new_dict = dict(self.__dict__)
-        new_dict["created_at"] = str(self.created_at.isoformat())
-        new_dict["updated_at"] = str(self.updated_at.isoformat())
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["__class__"] = self.__class__.__name__
         return (new_dict)

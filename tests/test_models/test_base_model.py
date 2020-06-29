e4cd51f41f658851ed9b9dc9ac73230be2277e92
andreshugueth <andreshugueth@gmail.com>
2020-06-29 11:44:54 -0500

    adding dictionary tests base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 import unittest
 import json
 import pep8
+import datetime
 
 from models.base_model import BaseModel
 
@@ -15,14 +16,14 @@ class TestBaseModel(unittest.TestCase):
         doc = BaseModel.__doc__
         self.assertGreater(len(doc), 1)
 
-    def test_pep8_conformance_rectangle(self):
+    def test_pep8_conformance_base_model(self):
         """Test that models/base_model.py conforms to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/base_model.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_pep8_conformance_test_rectangle(self):
+    def test_pep8_conformance_test_base_model(self):
         """Test that tests/test_models/test_base_model.py conforms to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         res = pep8style.check_files(['tests/test_models/test_base_model.py'])
@@ -42,14 +43,38 @@ class TestBaseModel(unittest.TestCase):
         my_model.my_number = 89
         self.assertEqual(my_model.name, "Holberton")
         self.assertEqual(my_model.my_number, 89)
+        model_types_json = {
+            "my_number": int,
+            "name": str,
+            "__class__": str,
+            "updated_at": str,
+            "id": str,
+            "created_at": str
+        }
+        my_model_json = my_model.to_dict()
+        for key, value in model_types_json.items():
+            with self.subTest(key=key, value=value):
+                self.assertIn(key, my_model_json)
+                self.assertIs(type(my_model_json[key]), value)
 
     def test_base_types(self):
-            second_model = BaseModel()
-            self.assertIs(type(second_model), BaseModel)
-            second_model.name = "Andres"
-            second_model.my_number = 80
-            self.assertEqual(second_model.name, "Andres")
-            self.assertEqual(second_model.my_number, 80)
+        second_model = BaseModel()
+        self.assertIs(type(second_model), BaseModel)
+        second_model.name = "Andres"
+        second_model.my_number = 80
+        self.assertEqual(second_model.name, "Andres")
+        self.assertEqual(second_model.my_number, 80)
+        model_types = {
+            "my_number": int,
+            "name": str,
+            "updated_at": datetime.datetime,
+            "id": str,
+            "created_at": datetime.datetime
+            }
+        for key, value in model_types.items():
+            with self.subTest(key=key, value=value):
+                self.assertIn(key, second_model.__dict__)
+                self.assertIs(type(second_model.__dict__[key]), value)
 
 if __name__ == '__main__':
     unittest.main()

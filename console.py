fec0d299ccd925eeadd4e4269ae8ec7df5c647be
andreshugueth <andreshugueth@gmail.com>
2020-06-30 00:47:18 -0500

    managing new classes in console and FileStorage

--- console.py
+++ console.py
@@ -4,13 +4,26 @@ import cmd
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
     """Simple command processor example."""
     prompt = '(hbnb) '
 
-    classes = ["BaseModel", "User"]
+    classes = [
+        "BaseModel",
+        "User",
+        "Amenity",
+        "City",
+        "Place",
+        "Review",
+        "State"
+        ]
 
     def do_EOF(self, line):
         """

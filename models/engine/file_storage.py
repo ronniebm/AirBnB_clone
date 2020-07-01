2012afb34453287558801d212172fe1dc8e5bd69
ronniebm <ronnie.coding@gmail.com>
2020-07-01 10:36:10 -0500

    fixing line#38 key with format()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,7 +35,7 @@ class FileStorage():
         key [str] -- key format generated.
         """
         if obj:
-            key = obj.__class__.__name__ + "." + obj.id
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
             FileStorage.__objects[key] = obj
 
     def save(self):
